b53cd360261c1fd0ea085e744b9abfc503acf6c2
omarvides <omarvides@gmail.com>
2017-04-03 08:54:19 -0600

    Changed to stream mode

--- scripts/slack.js
+++ scripts/slack.js
@@ -1,8 +1,26 @@
 'use strict';
 require('dotenv').config();
 
-const twitterController = require('../controllers/twitter-controller');
+// const twitterController = require('../controllers/twitter-controller');
+const twitterClient = require('../clients/twitter-client');
+const filters = [' go ', 'golang', 'node', 'javascript', 'nodejs'];
 
 module.exports = function(robot) {
-    robot.hear(/(#)?sendremote(s)?(.*)|(remote(s)?\?)/, twitterController.new);
+    // robot.hear(/(#)?sendremote(s)?(.*)|(remote(s)?\?)/, twitterController.new);
+    const nodeStream = twitterClient.stream('user');
+
+    nodeStream.on('data', function(event) {
+      let shouldSend = false;
+        filters.forEach(function(filter) {
+            if(event.text.indexOf(filter) >= 0) {
+               shouldSend = true;
+            }
+        });
+      if (shouldSend) {
+        robot.send({ room: process.env.DEFAULT_CHANNEL }, event.text);
+      }
+    });
+    nodeStream.on('error', function(err) {
+        throw err;
+    });
 }
