5f7bf5d6036ea25cebd240d7fecda951213ccc65
omarvides <omarvides@gmail.com>
2017-04-03 19:52:52 -0600

    Refactor

--- controllers/twitter-controller.js
+++ controllers/twitter-controller.js
@@ -1,44 +1,27 @@
 'use strict';
-
 const twitterClient = require('../clients/twitter-client');
-const params = { screen_name: 'nodejs' };
+const nodeStream = twitterClient.stream('user');
+const topic = ['go', 'golang', 'node', 'javascript', 'nodejs', 'devops', 'part time', 'parttime', 'freelance', 'full stack', 'fullstack'];
+const exclude = ['bigdata', 'europe', 'php']
 
-module.exports.new = function (res) {
-    const array = res.message.text.split(" ");
-    if (array.length > 2) {
-      getTweets(res, array[2]);
+module.exports.New = function(robot) {
+  nodeStream.on('data', function(event) {
+    let shouldSend = false;
+    topic.forEach(function(topic) {
+        if(event.text.toLowerCase().indexOf(topic.toLowerCase()) >= 0) {
+            shouldSend = true;
+        }
+    });
+    exclude.forEach(function(exclude) {
+      if(event.text.toLowerCase().indexOf(exclude.toLowerCase()) >= 0) {
+          shouldSend = false;
+      }
+    });
+    if (shouldSend) {
+      robot.send({ room: process.env.DEFAULT_CHANNEL }, event.text);
     }
-    else {
-      getTweets(res);
-    }
-};
-
-function getTweets(res, criteria) {
-  twitterClient.get('statuses/home_timeline', params, function(err, tweets, response) {
-      if (err) {
-          console.log(err);
-          return res.send('Sorry an error ocurred');
-      }
-
-      if (criteria) {
-        tweets = tweets.filter(getFilter(criteria));
-      }
-      if (tweets.length > 0) {
-        const randomnumber = Math.floor(Math.random() * (tweets.length - 1));
-        res.send(`Here is one ${tweets[randomnumber].text}`);
-        return res.send(`I found ${tweets.length} tweets related to \`${criteria}\` remote jobs`);
-      }
-
-      return res.send(`Found ${tweets.length} jobs matching the search criteria: \`${criteria}\``);
-
   });
-}
-
-function getFilter(criteria) {
-  return function filter(tweet) {
-    if (tweet.text.indexOf(criteria) >= 0 ) {
-      return true;
-    }
-    return false;
-  }
-}
+  nodeStream.on('error', function(err) {
+      throw err;
+  });
+};
